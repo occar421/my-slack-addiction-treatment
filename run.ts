ff16cb6dbcdf0ffe835d1a1b00b617093f929c54
MasuqaT <occar@hotmail.co.jp>
2024-01-03 22:52:22 +0900

    Update use of semver

--- run.ts
+++ run.ts
@@ -1,6 +1,6 @@
 import {parseArgs} from "https://deno.land/std@0.210.0/cli/mod.ts";
 import {join} from "https://deno.land/std@0.210.0/path/mod.ts";
-import {compare} from "https://deno.land/std@0.210.0/semver/mod.ts";
+import {compare, parse, SemVer} from "https://deno.land/std@0.210.0/semver/mod.ts";
 import asar from "npm:@electron/asar@3.2.3";
 
 const args = parseArgs(Deno.args);
@@ -59,19 +59,19 @@ document.addEventListener('DOMContentLoaded', async function() {
 async function pickAppDir(slackDir: string) {
     const regex = /app-(\d+.\d+(.\d+)?)/;
 
-    const versions = [];
+    const versions: [string, SemVer][] = [];
     for await (const entry of Deno.readDir(slackDir)) {
         if (entry.isDirectory) {
             const match = regex.exec(entry.name);
             if (match) {
-                versions.push(match[1]);
+                versions.push([match[1], parse(match[1])]);
             }
         }
     }
 
-    versions.sort(compare);
-    const version = versions.pop();
-    const appName = `app-${version}`;
+    versions.sort((v1, v2) => compare(v1[1], v2[1]));
+    const version = versions.pop()!;
+    const appName = `app-${version[0]}`;
     console.info(`Use "${appName}".`);
 
     return join(slackDir, appName);
