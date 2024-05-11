3a93081be3d4172d550d29f6c30eb8f6fd121531
MasuqaT <occar@hotmail.co.jp>
2024-05-12 01:51:44 +0900

    Use default directory

--- run.ts
+++ run.ts
@@ -1,5 +1,6 @@
 import * as log from "https://deno.land/std@0.210.0/log/mod.ts";
-import {Command} from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
+import {join} from "https://deno.land/std@0.210.0/path/join.ts";
+import {Command, ValidationError} from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
 import {applyCommand} from "./commands/apply.ts";
 import {recoverCommand} from "./commands/recover.ts";
 
@@ -13,10 +14,22 @@ log.setup({
             handlers: ["console"]
         }
     }
-})
+});
 
 await new Command()
-    .globalOption("--slack-dir <string>", "Target Slack app directory.")
+    .globalOption("--slack-dir <string>", "Target Slack app directory.", {
+        default: () => {
+            if (Deno.build.os !== "windows") {
+                throw new Error(`Not supported on ${Deno.build.os}.`);
+            }
+
+            const localappdata = Deno.env.get("LOCALAPPDATA");
+            if (!localappdata) {
+                throw new ValidationError(`Unable to locate Slack app directory.`);
+            }
+            return join(localappdata, "slack")
+        }
+    })
     .action(function () {
         this.showHelp();
     })
